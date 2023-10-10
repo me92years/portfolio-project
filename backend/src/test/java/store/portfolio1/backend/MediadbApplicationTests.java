3d2bd64d40c63157352103612473102f8ad300dd
User <User@DESKTOP-AEURFAK>
2023-10-10 19:31:11 +0900

    update backend files

--- backend/src/test/java/store/portfolio1/backend/MediadbApplicationTests.java
+++ backend/src/test/java/store/portfolio1/backend/MediadbApplicationTests.java
@@ -1,10 +1,10 @@
-package store.portfolio1.videodb;
+package store.portfolio1.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class VideodbApplicationTests {
+class MediadbApplicationTests {
 
 	@Test
 	void contextLoads() {
