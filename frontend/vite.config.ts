95378de5d9610340fab31c60a51f28042bb08b18
User <User@DESKTOP-AEURFAK>
2023-11-06 02:47:31 +0900

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: false
-      }
-    }
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:8080',
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, ''),
+  //       secure: false
+  //     }
+  //   }
+  // },
 })
