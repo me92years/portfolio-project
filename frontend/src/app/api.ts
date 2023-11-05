6fdae3c434db9759273d6eba6453d3c176859230
User <User@DESKTOP-AEURFAK>
2023-11-06 04:37:31 +0900

    frontend update

--- frontend/src/app/api.ts
+++ frontend/src/app/api.ts
@@ -4,7 +4,7 @@ import { PostReqDto, PostResDto, UserPrincipal } from './types'
 const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://portfolio1.store/api',
+    baseUrl: '/api',
     prepareHeaders: (headers) => {
       headers.set('Content-Type', 'application/json')
     }
